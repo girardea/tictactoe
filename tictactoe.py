6fbd836eb2b91195adcedb3fbf313e2855d5ef28
Pierre Girardeau <pgirardeau@crossdata.tech>
2021-01-05 16:12:30 +0100

    Implemented QStrategy player.

--- tictactoe.py
+++ tictactoe.py
@@ -21,11 +21,13 @@ to the lower-right corner.
 """
 import argparse
 
+from typing import Dict
+
 import tqdm
 
 # Project modules
 from players import Strategy, Dummy, SmartStart, Human
-from utils import display, test_finish
+from utils import display, possible_move, test_finish
 
 
 def play(p1: Strategy, p2: Strategy, verbose: bool = False) -> str:
@@ -72,6 +74,42 @@ def play(p1: Strategy, p2: Strategy, verbose: bool = False) -> str:
     return
 
 
+class QStrategy(Strategy):
+    """Implements a state-action value based strategy"""
+
+    def __init__(self, Q: Dict):
+        """Needs the Q-function to work (see README.md)"""
+        self.Q = Q
+
+    def action(self, s: str, my_mark: str = "x") -> str:
+        """Finds best action."""
+        if my_mark == "o":
+            # switch "x" and "o"
+            s = s.replace("x", "y").replace("o", "x").replace("y", "o")
+
+        # find best action (be aware it could be unfeasible)
+        try:
+            d = self.Q["data"][s]
+        except KeyError:
+            # if no info for this state, use random
+            return Dummy().action(s, my_mark)
+
+        no_feasible_move = True
+        move = -1
+        for move in sorted(d, key=d.get, reverse=True):
+            # convert from str to int (and to base 0 instead of base 1)
+            move = int(move) - 1
+
+            # check feasibility
+            if possible_move(s, move):
+                no_feasible_move = False
+                break
+
+        assert not no_feasible_move, f"Could not find a feasible move for board {s}"
+
+        return s[:move] + my_mark + s[move + 1 :]
+
+
 def load(player_name: str):
     """Returns a Strategy instance based on the name
     given as an input
@@ -83,7 +121,18 @@ def load(player_name: str):
     elif player_name == "me":
         return Human()
     else:
-        raise NotImplementedError(f"Could not find {player_name} player.")
+        # look for name in players folder
+        try:
+            import json
+
+            with open("players/" + player_name + ".json") as f:
+                Q = json.load(f)
+
+            return QStrategy(Q)
+        except FileNotFoundError:
+            raise FileNotFoundError(f"Could not find {player_name + '.json'} file.")
+        except:
+            raise NotImplementedError(f"Could not find {player_name} player.")
 
 
 if __name__ == "__main__":
