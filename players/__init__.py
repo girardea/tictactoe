0bc77c6cb8314c8b107b9573d9c2b10bcf4e7e42
Pierre Girardeau <pgirardeau@crossdata.tech>
2021-01-06 14:38:40 +0100

    Moved QStrategy to players init.py file.

--- players/__init__.py
+++ players/__init__.py
@@ -1,5 +1,7 @@
 """All tic-tac-toe strategies are placed here.
 """
+# Low-level modules
+from typing import Dict
 
 # Project modules
 from utils import display, possible_move
@@ -81,4 +83,37 @@ class Human(Strategy):
         return s[:move] + my_mark + s[move + 1 :]
 
 
-DEFAULT_PLAYERS = [Dummy(), SmartStart()]
+class QStrategy(Strategy):
+    """Implements a state-action value based strategy"""
+
+    def __init__(self, Q: Dict):
+        """Needs the Q-function to work (see README.md)"""
+        self.Q = Q
+
+    def action(self, s: str, my_mark: str = "x") -> str:
+        """Finds best action."""
+        if my_mark == "o":
+            # switch "x" and "o"
+            s = s.replace("x", "y").replace("o", "x").replace("y", "o")
+
+        # find best action (be aware it could be unfeasible)
+        try:
+            d = self.Q["data"][s]
+        except KeyError:
+            # if no info for this state, use random
+            return Dummy().action(s, my_mark)
+
+        no_feasible_move = True
+        move = -1
+        for move in sorted(d, key=d.get, reverse=True):
+            # convert from str to int (and to base 0 instead of base 1)
+            move = int(move) - 1
+
+            # check feasibility
+            if possible_move(s, move):
+                no_feasible_move = False
+                break
+
+        assert not no_feasible_move, f"Could not find a feasible move for board {s}"
+
+        return s[:move] + my_mark + s[move + 1 :]
